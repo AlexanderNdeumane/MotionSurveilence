aad9a3c60593f2addeefb573974f2fb89f0d2b78
Alexander <alexndeumane@gmail.com>
2020-08-18 18:36:42 +0200

    Add project files.

---
+++ MotionSurveilence/MotionForm.Designer.cs
@@ -0,0 +1,209 @@
+﻿namespace MotionSurveilence
+{
+    partial class MotionForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.buttonConnect = new System.Windows.Forms.Button();
+            this.buttonCompose = new System.Windows.Forms.Button();
+            this.cameraUrl = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.labelState = new System.Windows.Forms.Label();
+            this.videoViewerWF1 = new Ozeki.Media.VideoViewerWF();
+            this.groupBox2 = new System.Windows.Forms.GroupBox();
+            this.chck_box_detect = new System.Windows.Forms.CheckBox();
+            this.StartRecording = new System.Windows.Forms.Button();
+            this.StopRecording = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.groupBox2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.buttonDisconnect);
+            this.groupBox1.Controls.Add(this.buttonConnect);
+            this.groupBox1.Controls.Add(this.buttonCompose);
+            this.groupBox1.Controls.Add(this.cameraUrl);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Location = new System.Drawing.Point(31, 28);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(320, 117);
+            this.groupBox1.TabIndex = 2;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Connection";
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Location = new System.Drawing.Point(130, 66);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonDisconnect.TabIndex = 4;
+            this.buttonDisconnect.Text = "Disconnect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // buttonConnect
+            // 
+            this.buttonConnect.Location = new System.Drawing.Point(9, 66);
+            this.buttonConnect.Name = "buttonConnect";
+            this.buttonConnect.Size = new System.Drawing.Size(75, 23);
+            this.buttonConnect.TabIndex = 3;
+            this.buttonConnect.Text = "Connect";
+            this.buttonConnect.UseVisualStyleBackColor = true;
+            this.buttonConnect.Click += new System.EventHandler(this.buttonConnect_Click);
+            // 
+            // buttonCompose
+            // 
+            this.buttonCompose.Location = new System.Drawing.Point(227, 38);
+            this.buttonCompose.Name = "buttonCompose";
+            this.buttonCompose.Size = new System.Drawing.Size(75, 23);
+            this.buttonCompose.TabIndex = 2;
+            this.buttonCompose.Text = "Compose";
+            this.buttonCompose.UseVisualStyleBackColor = true;
+            this.buttonCompose.Click += new System.EventHandler(this.buttonCompose_Click);
+            // 
+            // cameraUrl
+            // 
+            this.cameraUrl.Location = new System.Drawing.Point(9, 40);
+            this.cameraUrl.Name = "cameraUrl";
+            this.cameraUrl.Size = new System.Drawing.Size(196, 20);
+            this.cameraUrl.TabIndex = 1;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(6, 19);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 13);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Camre URL:";
+            // 
+            // labelState
+            // 
+            this.labelState.AutoSize = true;
+            this.labelState.Location = new System.Drawing.Point(28, 148);
+            this.labelState.Name = "labelState";
+            this.labelState.Size = new System.Drawing.Size(35, 13);
+            this.labelState.TabIndex = 5;
+            this.labelState.Text = "State:";
+            // 
+            // videoViewerWF1
+            // 
+            this.videoViewerWF1.BackColor = System.Drawing.Color.Black;
+            this.videoViewerWF1.ContextMenuEnabled = true;
+            this.videoViewerWF1.FlipMode = Ozeki.Media.FlipMode.None;
+            this.videoViewerWF1.FrameStretch = Ozeki.Media.FrameStretch.Uniform;
+            this.videoViewerWF1.FullScreenEnabled = true;
+            this.videoViewerWF1.Location = new System.Drawing.Point(31, 165);
+            this.videoViewerWF1.Name = "videoViewerWF1";
+            this.videoViewerWF1.RotateAngle = 0;
+            this.videoViewerWF1.Size = new System.Drawing.Size(320, 240);
+            this.videoViewerWF1.TabIndex = 3;
+            this.videoViewerWF1.Text = "videoViewerWF1";
+            // 
+            // groupBox2
+            // 
+            this.groupBox2.Controls.Add(this.StopRecording);
+            this.groupBox2.Controls.Add(this.StartRecording);
+            this.groupBox2.Controls.Add(this.chck_box_detect);
+            this.groupBox2.Location = new System.Drawing.Point(418, 28);
+            this.groupBox2.Name = "groupBox2";
+            this.groupBox2.Size = new System.Drawing.Size(352, 117);
+            this.groupBox2.TabIndex = 4;
+            this.groupBox2.TabStop = false;
+            this.groupBox2.Text = "Controllers";
+            // 
+            // chck_box_detect
+            // 
+            this.chck_box_detect.AutoSize = true;
+            this.chck_box_detect.Location = new System.Drawing.Point(7, 20);
+            this.chck_box_detect.Name = "chck_box_detect";
+            this.chck_box_detect.Size = new System.Drawing.Size(93, 17);
+            this.chck_box_detect.TabIndex = 0;
+            this.chck_box_detect.Text = "Detect Motion";
+            this.chck_box_detect.UseVisualStyleBackColor = true;
+            this.chck_box_detect.CheckedChanged += new System.EventHandler(this.chck_box_detect_CheckedChanged);
+            // 
+            // StartRecording
+            // 
+            this.StartRecording.Location = new System.Drawing.Point(7, 37);
+            this.StartRecording.Name = "StartRecording";
+            this.StartRecording.Size = new System.Drawing.Size(112, 23);
+            this.StartRecording.TabIndex = 1;
+            this.StartRecording.Text = "Start Recording";
+            this.StartRecording.UseVisualStyleBackColor = true;
+            this.StartRecording.Click += new System.EventHandler(this.StartRecording_Click);
+            // 
+            // StopRecording
+            // 
+            this.StopRecording.Location = new System.Drawing.Point(7, 66);
+            this.StopRecording.Name = "StopRecording";
+            this.StopRecording.Size = new System.Drawing.Size(112, 23);
+            this.StopRecording.TabIndex = 2;
+            this.StopRecording.Text = "Stop Recording";
+            this.StopRecording.UseVisualStyleBackColor = true;
+            this.StopRecording.Click += new System.EventHandler(this.StopRecording_Click);
+            // 
+            // MotionForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelState);
+            this.Controls.Add(this.groupBox2);
+            this.Controls.Add(this.videoViewerWF1);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "MotionForm";
+            this.Text = "MotionForm";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.groupBox2.ResumeLayout(false);
+            this.groupBox2.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Label labelState;
+        private System.Windows.Forms.Button buttonDisconnect;
+        private System.Windows.Forms.Button buttonConnect;
+        private System.Windows.Forms.Button buttonCompose;
+        private System.Windows.Forms.TextBox cameraUrl;
+        private System.Windows.Forms.Label label1;
+        private Ozeki.Media.VideoViewerWF videoViewerWF1;
+        private System.Windows.Forms.GroupBox groupBox2;
+        private System.Windows.Forms.CheckBox chck_box_detect;
+        private System.Windows.Forms.Button StopRecording;
+        private System.Windows.Forms.Button StartRecording;
+    }
+}
---
+++ MotionSurveilence/MotionForm.cs
@@ -0,0 +1,160 @@
+﻿using Ozeki.Camera;
+using System;
+using System.Windows.Forms;
+using Ozeki.Vision;
+using Ozeki.Media;
+
+namespace MotionSurveilence
+{
+    public partial class MotionForm : Form
+    {
+        private OzekiCamera _webCamera;
+        private Ozeki.Media.DrawingImageProvider _imageProvider;
+        private Ozeki.Media.MediaConnector _mediaConnector;
+        private MPEG4Recorder _recorder;
+        private Ozeki.Media.CameraURLBuilderWF _myCameraUrlBuilder;
+        ImageProcesserHandler _imageProcesserHandler;
+        IMotionDetector _detector;
+        //
+        //Constructor for motion form
+        //
+        public MotionForm()
+        {
+            InitializeComponent();
+
+            _imageProvider = new Ozeki.Media.DrawingImageProvider();
+            _mediaConnector = new Ozeki.Media.MediaConnector();
+            var data = new Ozeki.Media.CameraURLBuilderData { DeviceTypeFilter = Ozeki.Media.DiscoverDeviceType.Both };
+            _myCameraUrlBuilder = new Ozeki.Media.CameraURLBuilderWF(data);
+
+            _detector = ImageProcesserFactory.CreateMotionDetector();
+            _imageProcesserHandler = new ImageProcesserHandler();
+            _imageProcesserHandler.AddProcesser(_detector);
+
+            videoViewerWF1.SetImageProvider(_imageProvider);
+        }
+
+        private void InvokeGuiThread(Action action)
+        {
+            BeginInvoke(action);
+        }
+
+        void _webCamera_CameraStateChanged(object sender, CameraStateEventArgs e)
+        {
+            InvokeGuiThread(() =>
+            {
+                switch (e.State)
+                {
+                    case CameraState.Streaming:
+                        buttonDisconnect.Enabled = true;
+                        break;
+                    case CameraState.Disconnected:
+                        buttonDisconnect.Enabled = false;
+                        buttonConnect.Enabled = true;
+                        break;
+                }
+            });
+
+            InvokeGuiThread(() =>
+            {
+                labelState.Text = e.State.ToString();
+            });
+        }
+        
+         //
+        //Brings up dialog to find camera
+        //
+        private void buttonCompose_Click(object sender, EventArgs e)
+        {
+            var result = _myCameraUrlBuilder.ShowDialog();
+
+            if (result != DialogResult.OK)
+                return;
+
+            cameraUrl.Text = _myCameraUrlBuilder.CameraURL;
+
+            buttonConnect.Enabled = true;
+        }
+        //
+        //Connects to camera url specified in the camera url text box
+        //
+        private void buttonConnect_Click(object sender, EventArgs e)
+        {
+            {
+                if (_webCamera != null)
+                {
+                    videoViewerWF1.Stop();
+                    _webCamera.Stop();
+                    _mediaConnector.Disconnect(_webCamera.VideoChannel, _imageProvider);
+                }
+                buttonConnect.Enabled = false;
+
+                _webCamera = new OzekiCamera(_myCameraUrlBuilder.CameraURL);
+
+                _webCamera.CameraStateChanged += _webCamera_CameraStateChanged;
+
+                _mediaConnector.Connect(_webCamera.VideoChannel, _imageProcesserHandler);
+                _mediaConnector.Connect(_imageProcesserHandler, _imageProvider);
+
+                _webCamera.Start();
+
+                videoViewerWF1.Start();
+                
+                
+            }
+        }
+        //
+        //Disconnects from the camera
+        //
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            videoViewerWF1.Stop();
+            _webCamera.Stop();
+            _mediaConnector.Disconnect(_webCamera.VideoChannel, _imageProvider);
+            _webCamera = null;
+        }
+        //
+        //Check box. Shows motion detection when checked and 
+        //does not show motion detection when unchecked
+        //
+        private void chck_box_detect_CheckedChanged(object sender, EventArgs e)
+        {
+            if (chck_box_detect.Checked)
+            {
+                _imageProcesserHandler.Start();
+            }
+            else
+            {
+                _imageProcesserHandler.Stop();
+            }
+           
+        }
+
+        void _recorder_MultiplexFinished(object sender, VoIPEventArgs<bool> e)
+        {
+            _recorder.MultiplexFinished -= _recorder_MultiplexFinished;
+            _recorder.Dispose();
+        }
+
+        private void StartRecording_Click(object sender, EventArgs e)
+        {
+            if (_webCamera.VideoChannel == null) return;
+            var date = DateTime.Now.Year + "-" + DateTime.Now.Month + "-" + DateTime.Now.Day + "-" +
+                        DateTime.Now.Hour + "-" + DateTime.Now.Minute + "-" + DateTime.Now.Second;
+
+            var currentpath = AppDomain.CurrentDomain.BaseDirectory + date + ".mpeg4";
+
+            _recorder = new MPEG4Recorder(currentpath);
+            _recorder.MultiplexFinished += _recorder_MultiplexFinished;
+
+            _mediaConnector.Connect(_webCamera.VideoChannel, _recorder.VideoRecorder);
+        }
+
+        private void StopRecording_Click(object sender, EventArgs e)
+        {
+            if (_webCamera.VideoChannel == null) return;
+            _mediaConnector.Disconnect(_webCamera.VideoChannel, _recorder.VideoRecorder);
+            _recorder.Multiplex();
+        }
+    }
+}
